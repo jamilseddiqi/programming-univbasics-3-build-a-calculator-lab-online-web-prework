9b990b000c4fcf7099974be4064a3d8c66e88133
Jamil Seddiqi <jamilsediq@gmail.com>
2019-11-13 17:11:40 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -29,24 +29,3 @@ puts "num1 * num2"
 end
 
 multiplication
-
-def modulo (num1, num2)
-
-modulo = num1 - num2
-
-puts "num1 - num2"
-
-end
-
-modulo
-
-
-def squares (num1, num2)
-
-subtraction = num1 - num2
-
-puts "num1 - num2"
-
-end
-
-square_root
