7f033d7ca8e8bc460fd4b025e7acbba4a0b0dcb7
Jamil Seddiqi <jamilsediq@gmail.com>
2019-11-13 17:14:04 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,31 @@
-def addition(num1, num2)
+
+def subtraction (num1, num2)
+
+subtraction = num1 - num2
+
+puts "num1 - num2"
 
 end
 
-def subtraction(num1, num2)
+subtraction
+
+def division (num1, num2)
+
+division = num1 / num2
+
+puts "num1 / num2"
 
 end
 
-def division(num1, num2)
+division
+
+
+def multiplication (num1, num2)
+
+multiplication = num1 * num2
+
+puts "num1 * num2"
 
 end
 
-def multiplication(num1, num2)
-
-end
-
-def modulo(num1, num2)
-
-end
-
-def square_root(num)
-
-end
+multiplication
